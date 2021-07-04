0837a262038dcebfb38d9748b0795004f90c70df
yaozm <ityaozm@gmail.com>
2021-07-04 15:26:57 +0800

    Optimize SendExceptionNotification job

--- src/Jobs/SendExceptionNotification.php
+++ src/Jobs/SendExceptionNotification.php
@@ -17,8 +17,9 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
+use Throwable;
 
-class ExceptionMessageSendJob implements ShouldQueue
+class SendExceptionNotification implements ShouldQueue
 {
     use Dispatchable;
     use InteractsWithQueue;
@@ -26,12 +27,33 @@ class ExceptionMessageSendJob implements ShouldQueue
     use SerializesModels;
 
     /**
+     * 在超时之前任务可以运行的秒数.
+     *
+     * @var int
+     */
+    public $timeout = 30;
+
+    /**
+     * 任务可尝试的次数.
+     *
+     * @var int
+     */
+    public $tries = 3;
+
+    /**
+     * 任务失败前允许的最大异常数.
+     *
+     * @var int
+     */
+    public $maxExceptions = 3;
+
+    /**
      * @var \Guanguans\Notify\Clients\Client
      */
     protected $client;
 
     /**
-     * ExceptionMessageSendJob constructor.
+     * SendExceptionNotification constructor.
      */
     public function __construct(Client $client)
     {
@@ -46,5 +68,21 @@ class ExceptionMessageSendJob implements ShouldQueue
         $response = $this->client->send();
 
         config('exception-notify.debug') and Log::debug('Exception notify debugging: ', $response);
+    }
+
+    /**
+     * 任务未能处理.
+     */
+    public function failed(Throwable $exception)
+    {
+        Log::error($exception->getMessage());
+    }
+
+    /**
+     * 计算在重试任务之前需等待的秒数.
+     */
+    public function backoff()
+    {
+        return [1, 10, 30];
     }
 }
