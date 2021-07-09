2ec0478e2a43f47d76e4cb8b8a6c172a56bd9f57
yaozm <ityaozm@gmail.com>
2021-07-09 15:38:40 +0800

    Update php-cs-fixer config file

---
+++ .php_cs.dist.php
@@ -0,0 +1,11 @@
+<?php
+
+$header = <<<EOF
+This file is part of the guanguans/laravel-exception-notify.
+
+(c) guanguans <ityaozm@gmail.com>
+
+This source file is subject to the MIT license that is bundled.
+EOF;
+
+return require __DIR__.'/.php_cs.dist';
