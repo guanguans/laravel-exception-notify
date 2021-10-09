0aa753421001660bfde0f0c19083dfe81dab18f3
Wenzhou Chan <wenzhouchan@gmail.com>
2021-10-09 09:55:39 +0800

    Fix 删除 `$dispatch->afterResponse();`，解决在某些情况下队列仍是同步执行
    即使 `.env` 里配置的 `QUEUE_CONNECTION=redis`，也无效

--- src/Notifier.php
+++ src/Notifier.php
@@ -111,7 +111,7 @@ md;
                 })
             ));
 
-            method_exists($dispatch, 'afterResponse') && $dispatch->afterResponse();
+            // method_exists($dispatch, 'afterResponse') && $dispatch->afterResponse();
         } catch (Throwable $e) {
             Log::error($e->getMessage());
         }
