c2cd7dff9bad3d6e5efaf7e6b4a509d961f16ea1
yaozm <ityaozm@gmail.com>
2023-08-03 17:57:09 +0800

    refactor(src): Refactor ExceptionNotifyServiceProvider registerCollectorManager method
    - Simplify the registration of CollectorManager using a lambda function
    - Use the config helper function to access the collectors configuration
    - Map and instantiate the collectors using the container
    - Alias the CollectorManager class

--- src/ExceptionNotifyServiceProvider.php
+++ src/ExceptionNotifyServiceProvider.php
@@ -29,29 +29,39 @@ class ExceptionNotifyServiceProvider extends ServiceProvider
 {
     protected bool $defer = false;
 
-    public function boot(): void
-    {
-        $this->setupConfig();
-        Request::mixin($this->app->make(RequestMacro::class));
-        Collection::mixin($this->app->make(CollectionMacro::class));
-        Str::mixin($this->app->make(StrMacro::class));
-        Stringable::mixin($this->app->make(StringableMacro::class));
-    }
-
     public function register(): void
     {
-        // Adapt lumen
         $this->setupConfig();
+        $this->registerMacros();
         $this->registerExceptionNotifyManager();
         $this->registerCollectorManager();
     }
 
-    public function provides()
+    public function boot(): void
+    {
+        $this->setupConfig();
+    }
+
+    public function provides(): array
     {
         return [
+            $this->toAlias(CollectorManager::class),
+            $this->toAlias(ExceptionNotifyManager::class),
+            CollectorManager::class,
             ExceptionNotifyManager::class,
-            'exception.notify',
         ];
+    }
+
+    /**
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     * @throws \ReflectionException
+     */
+    public function registerMacros(): void
+    {
+        Request::mixin($this->app->make(RequestMacro::class));
+        Collection::mixin($this->app->make(CollectionMacro::class));
+        Str::mixin($this->app->make(StrMacro::class));
+        Stringable::mixin($this->app->make(StringableMacro::class));
     }
 
     /**
@@ -74,31 +84,56 @@ class ExceptionNotifyServiceProvider extends ServiceProvider
         $this->mergeConfigFrom($source, 'exception-notify');
     }
 
-    protected function registerCollectorManager(): void
-    {
-        $this->app->singleton(CollectorManager::class, function (Container $container): CollectorManager {
-            /** @var array<\Guanguans\LaravelExceptionNotify\Contracts\CollectorContract> $collection */
-            $collection = collect($container['config']['exception-notify.collectors'])
-                ->map(function ($parameters, $class) {
-                    if (! \is_array($parameters)) {
-                        [$parameters, $class] = [(array) $class, $parameters];
-                    }
-
-                    return $this->app->make($class, $parameters);
-                })
-                ->values()
-                ->all();
-
-            return new CollectorManager($collection);
-        });
-
-        $this->app->alias(CollectorManager::class, 'exception.collector');
-    }
-
     protected function registerExceptionNotifyManager(): void
     {
-        $this->app->singleton(ExceptionNotifyManager::class, static fn ($app): ExceptionNotifyManager => new ExceptionNotifyManager($app));
+        $this->app->singleton(
+            ExceptionNotifyManager::class,
+            static fn (Container $container): ExceptionNotifyManager => new ExceptionNotifyManager($container)
+        );
 
-        $this->app->alias(ExceptionNotifyManager::class, 'exception.notify');
+        $this->alias(ExceptionNotifyManager::class);
+    }
+
+    protected function registerCollectorManager(): void
+    {
+        $this->app->singleton(
+            CollectorManager::class,
+            static fn (Container $container): CollectorManager => new CollectorManager(
+                collect(config('exception-notify.collectors', []))
+                    ->map(static function ($parameters, $class) use ($container) {
+                        if (! \is_array($parameters)) {
+                            [$parameters, $class] = [(array) $class, $parameters];
+                        }
+
+                        return $container->make($class, $parameters);
+                    })
+                    ->all()
+            )
+        );
+
+        $this->alias(CollectorManager::class);
+    }
+
+    /**
+     * @param class-string $class
+     */
+    protected function alias(string $class, ?string $prefix = null): void
+    {
+        $this->app->alias($class, $this->toAlias($class, $prefix));
+    }
+
+    /**
+     * @param class-string $class
+     */
+    protected function toAlias(string $class, ?string $prefix = null): string
+    {
+        $prefix ??= 'exception-notify.';
+
+        $alias = Str::snake(class_basename($class), '.');
+        if (Str::startsWith($alias, Str::replaceLast('.', '', $prefix))) {
+            return $alias;
+        }
+
+        return $prefix.$alias;
     }
 }
