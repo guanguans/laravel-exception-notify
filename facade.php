c54ca21da9484a361e7d2c4f5bc755e96b3dee6c
yaozm <ityaozm@gmail.com>
2023-08-29 15:11:18 +0800

    chore(facade): Update vendor/bin/facade.php path
    - Updated require statement for vendor/bin/facade.php

--- facade.php
+++ facade.php
@@ -11,8 +11,5 @@ declare(strict_types=1);
  */
 
 require __DIR__.'/vendor/autoload.php';
-$_composer_autoload_path = __DIR__.'/vendor-bin/facade-documenter/vendor/autoload.php';
 
-// require __DIR__.'/src/Facades/ExceptionNotify.php';
-
-require __DIR__.'/vendor-bin/facade-documenter/vendor/laravel/facade-documenter/facade.php';
+require __DIR__.'/vendor/bin/facade.php';
