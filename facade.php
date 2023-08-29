537e083c47bea46bc12e22910979fb8c8d7e10c3
yaozm <ityaozm@gmail.com>
2023-08-29 14:36:30 +0800

    feat(facade): Add facade.php file
    - Add 'facade.php' file with necessary code
    - Require 'vendor/autoload.php' and 'vendor-bin/facade-documenter/vendor/autoload.php'

---
+++ facade.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/laravel-exception-notify.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+require __DIR__.'/vendor/autoload.php';
+$_composer_autoload_path = __DIR__.'/vendor-bin/facade-documenter/vendor/autoload.php';
+
+// require __DIR__.'/src/Facades/ExceptionNotify.php';
+
+require __DIR__.'/vendor-bin/facade-documenter/vendor/laravel/facade-documenter/facade.php';
