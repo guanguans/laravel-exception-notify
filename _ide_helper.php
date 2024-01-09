a3f4438c8a474434417c6d9634b1c7bfc2a4ffc3
yaozm <ityaozm@gmail.com>
2024-01-09 11:39:30 +0800

    chore(deps): update PHP dependencies
    - Update fully_qualified_strict_types in .php-cs-fixer.php
    - Update path to PHP binary in composer.json lint scripts
    - Update Psalm version in psalm-baseline.xml

--- _ide_helper.php
+++ _ide_helper.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  */
 
 namespace {
-    class ExceptionNotify extends \Guanguans\LaravelExceptionNotify\Facades\ExceptionNotify {}
+    class ExceptionNotify extends Guanguans\LaravelExceptionNotify\Facades\ExceptionNotify {}
 }
 
 namespace Illuminate\Support {
