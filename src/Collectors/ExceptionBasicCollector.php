b30ac1107ec5a627b8ed7083105697e67a248e8d
yaozm <ityaozm@gmail.com>
2024-01-11 16:19:37 +0800

    Refactor(ExceptionBasicCollector): Improve exception data collection
    - Add link to Symfony FlattenExceptionNormalizer
    - Include status code, status text, and headers in collected data

--- src/Collectors/ExceptionBasicCollector.php
+++ src/Collectors/ExceptionBasicCollector.php
@@ -14,14 +14,20 @@ namespace Guanguans\LaravelExceptionNotify\Collectors;
 
 class ExceptionBasicCollector extends ExceptionCollector
 {
+    /**
+     * @@see https://github.com/symfony/symfony/blob/7.1/src/Symfony/Component/Messenger/Transport/Serialization/Normalizer/FlattenExceptionNormalizer.php
+     */
     public function collect(): array
     {
         return [
-            'class' => \get_class($this->exception),
             'message' => $this->exception->getMessage(),
             'code' => $this->exception->getCode(),
+            'class' => \get_class($this->exception),
             'file' => $this->exception->getFile(),
             'line' => $this->exception->getLine(),
+            'status_code' => $this->flattenException->getStatusCode(),
+            'status_text' => $this->flattenException->getStatusText(),
+            'headers' => $this->flattenException->getHeaders(),
         ];
     }
 }
