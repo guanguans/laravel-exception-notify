afbd0ff9710d1bec1f0d8ac8f30f46c06daef31b
yaozm <ityaozm@gmail.com>
2024-04-18 18:23:17 +0800

    refactor(NotifyChannel): Refactor NotifyChannel class for better readability and maintainability
    - Refactored NotifyChannel class to use more descriptive variable names and improve code structure
    - Updated NotifyChannel constructor to accept an array of configuration options
    - Added createClient method to dynamically create client based on configuration
    - Added createMessage method to dynamically create message based on configuration

--- src/Channels/NotifyChannel.php
+++ src/Channels/NotifyChannel.php
@@ -14,22 +14,52 @@ declare(strict_types=1);
 namespace Guanguans\LaravelExceptionNotify\Channels;
 
 use Guanguans\LaravelExceptionNotify\Contracts\ChannelContract;
-use Guanguans\Notify\Clients\Client;
-use Guanguans\Notify\Contracts\MessageInterface;
+use Guanguans\Notify\Foundation\Contracts\Client;
+use Guanguans\Notify\Foundation\Contracts\Message;
+use GuzzleHttp\UriTemplate\UriTemplate;
+use Illuminate\Config\Repository;
 
-abstract class NotifyChannel implements ChannelContract
+class NotifyChannel implements ChannelContract
 {
-    protected Client $client;
+    private Repository $configRepository;
 
-    public function __construct(Client $client)
+    public function __construct(array $config)
     {
-        $this->client = $client;
+        $this->configRepository = new Repository($config);
     }
 
     final public function report(string $report)
     {
-        return $this->client->send($this->createMessage($report));
+        return $this->createClient()->send($this->createMessage($report));
     }
 
-    abstract protected function createMessage(string $report): MessageInterface;
+    private function createClient(): Client
+    {
+        /** @var \Guanguans\Notify\Foundation\Contracts\Authenticator $authenticator */
+        $authenticator = make($this->configRepository->get('authenticator'));
+
+        /** @var Client $client */
+        // $client = new ($this->configRepository->get('client'))($authenticator);
+        if ($this->configRepository->has('client_options')) {
+            $client->setHttpOptions($this->configRepository->get('client_options'));
+        }
+
+        return $client;
+    }
+
+    private function createMessage(string $report): Message
+    {
+        $options = array_map(static function ($value) use ($report) {
+            if (\is_string($value)) {
+                return UriTemplate::expand($value, [
+                    'title' => config('exception-notify.title'),
+                    'report' => $report,
+                ]);
+            }
+
+            return $value;
+        }, $this->configRepository->get('message.options'));
+
+        // return new ($this->configRepository->get('message.class'))($options);
+    }
 }
