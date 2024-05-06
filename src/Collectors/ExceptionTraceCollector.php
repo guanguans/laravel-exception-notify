b465ab9906a9c1e5d9d2ece25be770b29f4ecf2a
yaozm <ityaozm@gmail.com>
2024-05-06 18:18:40 +0800

    refactor(collector): Improve exception trace collection
    - Remove vendor traces from the collected exception trace
    - Replace base path with an empty string in the collected trace

--- src/Collectors/ExceptionTraceCollector.php
+++ src/Collectors/ExceptionTraceCollector.php
@@ -21,6 +21,7 @@ class ExceptionTraceCollector extends ExceptionCollector
     {
         return collect(explode(\PHP_EOL, $this->exception->getTraceAsString()))
             ->filter(static fn ($trace): bool => !Str::contains($trace, 'vendor'))
+            ->map(static fn ($trace): string => (string) str($trace)->replaceFirst(base_path(), ''))
             ->all();
     }
 }
