047280c1119509d03dbb0ffffcf8e2b8c7bedf19
guanguans <ityaozm@gmail.com>
2025-03-05 19:09:03 +0800

    feat(traits): Add MakeStaticable, SetStateable, and WithPipeArgs
    - Introduce MakeStaticable trait for creating static instances
    - Implement SetStateable trait to support __set_state method
    - Add WithPipeArgs trait for formatting class names with arguments
    
    These traits enhance the functionality of the Laravel Exception Notify package.

---
+++ src/Support/Traits/SetStateable.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2021-2025 guanguans<ityaozm@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/guanguans/laravel-exception-notify
+ */
+
+namespace Guanguans\LaravelExceptionNotify\Support\Traits;
+
+trait SetStateable
+{
+    public static function __set_state(array $properties): self
+    {
+        return app(static::class, $properties);
+    }
+}
---
+++ src/Support/Traits/WithPipeArgs.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2021-2025 guanguans<ityaozm@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/guanguans/laravel-exception-notify
+ */
+
+namespace Guanguans\LaravelExceptionNotify\Support\Traits;
+
+trait WithPipeArgs
+{
+    public static function with(mixed ...$args): string
+    {
+        if ([] === $args) {
+            return static::class;
+        }
+
+        return static::class.':'.implode(',', $args);
+    }
+}
