5af4bc9f9bf2ba36a31e21826e4b79132a153a5c
guanguans <ityaozm@gmail.com>
2025-03-06 16:06:12 +0800

    refactor(HydratePipeFuncCallToStaticCallRector): Use ValueResolver for static call
    - Inject ValueResolver into the constructor for improved dependency management.
    - Replace direct node access with ValueResolver for better value handling.
    - Simplify static call creation by utilizing resolved values from arguments.

--- src/Support/Rectors/HydratePipeFuncCallToStaticCallRector.php
+++ src/Support/Rectors/HydratePipeFuncCallToStaticCallRector.php
@@ -15,6 +15,7 @@ namespace Guanguans\LaravelExceptionNotify\Support\Rectors;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\FuncCall;
+use Rector\PhpParser\Node\Value\ValueResolver;
 use Rector\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
@@ -24,6 +25,8 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
  */
 final class HydratePipeFuncCallToStaticCallRector extends AbstractRector
 {
+    public function __construct(private ValueResolver $valueResolver) {}
+
     /**
      * @throws \Symplify\RuleDocGenerator\Exception\PoorDocumentationException
      */
@@ -59,11 +62,8 @@ final class HydratePipeFuncCallToStaticCallRector extends AbstractRector
         if ($this->isName($node, 'Guanguans\LaravelExceptionNotify\Support\hydrate_pipe')) {
             $args = $node->getArgs();
 
-            /** @var Node\Expr\ClassConstFetch $classConstFetch */
-            $classConstFetch = $args[0]->value;
-
             return $this->nodeFactory->createStaticCall(
-                $this->getName($classConstFetch->class),
+                $this->valueResolver->getValue($args[0]),
                 'with',
                 \array_slice($args, 1)
             );
