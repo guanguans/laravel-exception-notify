5637cb6dd7fc4ae9806700e12ae1c2db363ca24b
guanguans <ityaozm@gmail.com>
2025-05-13 05:59:23 +0800

    fix(config): Update Notify namespace in exception-notify configuration
    - Changed the import path for Notify classes from Guanguans\Notify to Notify.
    - Updated the class references for various notification drivers including Bark, Chanify, DingTalk, Discord, Lark, Ntfy, PushDeer, Slack, Telegram, and WeWork.
    - This change ensures that the application uses the correct namespace for notification handling.
    
    Signed-off-by: guanguans <ityaozm@gmail.com>

--- config/exception-notify.php
+++ config/exception-notify.php
@@ -13,23 +13,12 @@ declare(strict_types=1);
  * @see https://github.com/guanguans/laravel-exception-notify
  */
 
-use Guanguans\LaravelExceptionNotify\Collectors\ApplicationCollector;
-use Guanguans\LaravelExceptionNotify\Collectors\ChoreCollector;
-use Guanguans\LaravelExceptionNotify\Collectors\ExceptionBasicCollector;
-use Guanguans\LaravelExceptionNotify\Collectors\ExceptionContextCollector;
-use Guanguans\LaravelExceptionNotify\Collectors\ExceptionTraceCollector;
-use Guanguans\LaravelExceptionNotify\Collectors\RequestBasicCollector;
-use Guanguans\LaravelExceptionNotify\Collectors\RequestFileCollector;
-use Guanguans\LaravelExceptionNotify\Collectors\RequestHeaderCollector;
-use Guanguans\LaravelExceptionNotify\Collectors\RequestPostCollector;
-use Guanguans\LaravelExceptionNotify\Collectors\RequestQueryCollector;
+use Guanguans\LaravelExceptionNotify\Collectors;
 use Guanguans\LaravelExceptionNotify\Jobs\ReportExceptionJob;
 use Guanguans\LaravelExceptionNotify\Mail\ReportExceptionMail;
-use Guanguans\LaravelExceptionNotify\Pipes\AddKeywordChorePipe;
-use Guanguans\LaravelExceptionNotify\Pipes\LimitLengthPipe;
-use Guanguans\LaravelExceptionNotify\Pipes\SprintfHtmlPipe;
-use Guanguans\LaravelExceptionNotify\Pipes\SprintfMarkdownPipe;
+use Guanguans\LaravelExceptionNotify\Pipes;
 use Guanguans\LaravelExceptionNotify\Template;
+use Guanguans\Notify;
 use function Guanguans\LaravelExceptionNotify\Support\env_explode;
 
 return [
@@ -71,16 +60,16 @@ return [
      * The list of collector that report exception.
      */
     'collectors' => [
-        ApplicationCollector::class,
-        ChoreCollector::class,
-        RequestBasicCollector::class,
-        ExceptionBasicCollector::class,
-        ExceptionContextCollector::class,
-        ExceptionTraceCollector::class,
-        // RequestHeaderCollector::class,
-        // RequestQueryCollector::class,
-        // RequestPostCollector::class,
-        // RequestFileCollector::class,
+        Collectors\ApplicationCollector::class,
+        Collectors\ChoreCollector::class,
+        Collectors\RequestBasicCollector::class,
+        Collectors\ExceptionBasicCollector::class,
+        Collectors\ExceptionContextCollector::class,
+        Collectors\ExceptionTraceCollector::class,
+        // Collectors\RequestHeaderCollector::class,
+        // Collectors\RequestQueryCollector::class,
+        // Collectors\RequestPostCollector::class,
+        // Collectors\RequestFileCollector::class,
     ],
 
     /**
@@ -151,7 +140,7 @@ return [
                 ]),
             ],
             'pipes' => [
-                SprintfHtmlPipe::class,
+                Pipes\SprintfHtmlPipe::class,
             ],
         ],
 
@@ -165,15 +154,15 @@ return [
         // 'foo' => [
         //     'driver' => 'notify',
         //     'authenticator' => [
-        //         'class' => Guanguans\Notify\Foo\Authenticator::class,
+        //         'class' => Notify\Foo\Authenticator::class,
         //         'parameter1' => '...',
         //         // ...
         //     ],
         //     'client' => [
-        //         'class' => Guanguans\Notify\Foo\Client::class,
+        //         'class' => Notify\Foo\Client::class,
         //         /** @see \GuzzleHttp\RequestOptions */
         //         'http_options' => [],
-        //         'extender' => static fn (Guanguans\Notify\Foundation\Client $client) => $client->push(
+        //         'extender' => static fn (Notify\Foundation\Client $client) => $client->push(
         //             GuzzleHttp\Middleware::log(
         //                 Illuminate\Support\Facades\Log::channel(),
         //                 new GuzzleHttp\MessageFormatter(GuzzleHttp\MessageFormatter::DEBUG),
@@ -182,137 +171,137 @@ return [
         //         ),
         //     ],
         //     'message' => [
-        //         'class' => Guanguans\Notify\Foo\Messages\Message::class,
+        //         'class' => Notify\Foo\Messages\Message::class,
         //         'options' => [
         //             'option1' => Template::TITLE,
         //             'option2' => Template::CONTENT,
         //         ],
         //     ],
         //     'pipes' => [
-        //         LimitLengthPipe::with(1024),
+        //         Pipes\LimitLengthPipe::with(1024),
         //     ],
         // ],
 
         'bark' => [
             'driver' => 'notify',
             'authenticator' => [
-                'class' => Guanguans\Notify\Bark\Authenticator::class,
+                'class' => Notify\Bark\Authenticator::class,
                 'token' => env('EXCEPTION_NOTIFY_BARK_TOKEN'),
             ],
             'client' => [
-                'class' => Guanguans\Notify\Bark\Client::class,
+                'class' => Notify\Bark\Client::class,
             ],
             'message' => [
-                'class' => Guanguans\Notify\Bark\Messages\Message::class,
+                'class' => Notify\Bark\Messages\Message::class,
                 'options' => [
                     'title' => Template::TITLE,
                     'body' => Template::CONTENT,
                 ],
             ],
             'pipes' => [
-                LimitLengthPipe::with(4096),
+                Pipes\LimitLengthPipe::with(4096),
             ],
         ],
 
         'chanify' => [
             'driver' => 'notify',
             'authenticator' => [
-                'class' => Guanguans\Notify\Chanify\Authenticator::class,
+                'class' => Notify\Chanify\Authenticator::class,
                 'token' => env('EXCEPTION_NOTIFY_CHANIFY_TOKEN'),
             ],
             'client' => [
-                'class' => Guanguans\Notify\Chanify\Client::class,
+                'class' => Notify\Chanify\Client::class,
             ],
             'message' => [
-                'class' => Guanguans\Notify\Chanify\Messages\TextMessage::class,
+                'class' => Notify\Chanify\Messages\TextMessage::class,
                 'options' => [
                     'title' => Template::TITLE,
                     'text' => Template::CONTENT,
                 ],
             ],
             'pipes' => [
-                LimitLengthPipe::with(1024),
+                Pipes\LimitLengthPipe::with(1024),
             ],
         ],
 
         'dingTalk' => [
             'driver' => 'notify',
             'authenticator' => [
-                'class' => Guanguans\Notify\DingTalk\Authenticator::class,
+                'class' => Notify\DingTalk\Authenticator::class,
                 'token' => env('EXCEPTION_NOTIFY_DINGTALK_TOKEN'),
                 'secret' => env('EXCEPTION_NOTIFY_DINGTALK_SECRET'),
             ],
             'client' => [
-                'class' => Guanguans\Notify\DingTalk\Client::class,
+                'class' => Notify\DingTalk\Client::class,
             ],
             'message' => [
-                'class' => Guanguans\Notify\DingTalk\Messages\MarkdownMessage::class,
+                'class' => Notify\DingTalk\Messages\MarkdownMessage::class,
                 'options' => [
                     'title' => Template::TITLE,
                     'text' => Template::CONTENT,
                 ],
             ],
             'pipes' => [
-                AddKeywordChorePipe::with(env('EXCEPTION_NOTIFY_DINGTALK_KEYWORD')),
-                SprintfMarkdownPipe::class,
-                LimitLengthPipe::with(20000),
+                Pipes\AddKeywordChorePipe::with(env('EXCEPTION_NOTIFY_DINGTALK_KEYWORD')),
+                Pipes\SprintfMarkdownPipe::class,
+                Pipes\LimitLengthPipe::with(20000),
             ],
         ],
 
         'discord' => [
             'driver' => 'notify',
             'authenticator' => [
-                'class' => Guanguans\Notify\Discord\Authenticator::class,
+                'class' => Notify\Discord\Authenticator::class,
                 'webHook' => env('EXCEPTION_NOTIFY_DISCORD_WEBHOOK'),
             ],
             'client' => [
-                'class' => Guanguans\Notify\Discord\Client::class,
+                'class' => Notify\Discord\Client::class,
             ],
             'message' => [
-                'class' => Guanguans\Notify\Discord\Messages\Message::class,
+                'class' => Notify\Discord\Messages\Message::class,
                 'options' => [
                     'content' => Template::CONTENT,
                 ],
             ],
             'pipes' => [
-                LimitLengthPipe::with(2000),
+                Pipes\LimitLengthPipe::with(2000),
             ],
         ],
 
         'lark' => [
             'driver' => 'notify',
             'authenticator' => [
-                'class' => Guanguans\Notify\Lark\Authenticator::class,
+                'class' => Notify\Lark\Authenticator::class,
                 'token' => env('EXCEPTION_NOTIFY_LARK_TOKEN'),
                 'secret' => env('EXCEPTION_NOTIFY_LARK_SECRET'),
             ],
             'client' => [
-                'class' => Guanguans\Notify\Lark\Client::class,
+                'class' => Notify\Lark\Client::class,
             ],
             'message' => [
-                'class' => Guanguans\Notify\Lark\Messages\TextMessage::class,
+                'class' => Notify\Lark\Messages\TextMessage::class,
                 'options' => [
                     'text' => Template::CONTENT,
                 ],
             ],
             'pipes' => [
-                AddKeywordChorePipe::with(env('EXCEPTION_NOTIFY_LARK_KEYWORD')),
-                LimitLengthPipe::with(30720),
+                Pipes\AddKeywordChorePipe::with(env('EXCEPTION_NOTIFY_LARK_KEYWORD')),
+                Pipes\LimitLengthPipe::with(30720),
             ],
         ],
 
         'ntfy' => [
             'driver' => 'notify',
             'authenticator' => [
-                'class' => Guanguans\Notify\Ntfy\Authenticator::class,
+                'class' => Notify\Ntfy\Authenticator::class,
                 'usernameOrToken' => env('EXCEPTION_NOTIFY_NTFY_USERNAMEORTOKEN'),
                 'password' => env('EXCEPTION_NOTIFY_NTFY_PASSWORD'),
             ],
             'client' => [
-                'class' => Guanguans\Notify\Ntfy\Client::class,
+                'class' => Notify\Ntfy\Client::class,
             ],
             'message' => [
-                'class' => Guanguans\Notify\Ntfy\Messages\Message::class,
+                'class' => Notify\Ntfy\Messages\Message::class,
                 'options' => [
                     'topic' => env('EXCEPTION_NOTIFY_NTFY_TOPIC', 'laravel-exception-notify'),
                     'title' => Template::TITLE,
@@ -320,21 +309,21 @@ return [
                 ],
             ],
             'pipes' => [
-                LimitLengthPipe::with(4096),
+                Pipes\LimitLengthPipe::with(4096),
             ],
         ],
 
         'pushDeer' => [
             'driver' => 'notify',
             'authenticator' => [
-                'class' => Guanguans\Notify\PushDeer\Authenticator::class,
+                'class' => Notify\PushDeer\Authenticator::class,
                 'token' => env('EXCEPTION_NOTIFY_PUSHDEER_TOKEN'),
             ],
             'client' => [
-                'class' => Guanguans\Notify\PushDeer\Client::class,
+                'class' => Notify\PushDeer\Client::class,
             ],
             'message' => [
-                'class' => Guanguans\Notify\PushDeer\Messages\Message::class,
+                'class' => Notify\PushDeer\Messages\Message::class,
                 'options' => [
                     'type' => 'markdown',
                     'text' => Template::TITLE,
@@ -342,72 +331,72 @@ return [
                 ],
             ],
             'pipes' => [
-                SprintfMarkdownPipe::class,
-                // LimitLengthPipe::with(4096),
+                Pipes\SprintfMarkdownPipe::class,
+                // Pipes\LimitLengthPipe::with(4096),
             ],
         ],
 
         'slack' => [
             'driver' => 'notify',
             'authenticator' => [
-                'class' => Guanguans\Notify\Slack\Authenticator::class,
+                'class' => Notify\Slack\Authenticator::class,
                 'webHook' => env('EXCEPTION_NOTIFY_SLACK_WEBHOOK'),
             ],
             'client' => [
-                'class' => Guanguans\Notify\Slack\Client::class,
+                'class' => Notify\Slack\Client::class,
             ],
             'message' => [
-                'class' => Guanguans\Notify\Slack\Messages\Message::class,
+                'class' => Notify\Slack\Messages\Message::class,
                 'options' => [
                     'mrkdwn' => true,
                     'text' => Template::CONTENT,
                 ],
             ],
             'pipes' => [
-                SprintfMarkdownPipe::class,
-                // LimitLengthPipe::with(10240),
+                Pipes\SprintfMarkdownPipe::class,
+                // Pipes\LimitLengthPipe::with(10240),
             ],
         ],
 
         'telegram' => [
             'driver' => 'notify',
             'authenticator' => [
-                'class' => Guanguans\Notify\Telegram\Authenticator::class,
+                'class' => Notify\Telegram\Authenticator::class,
                 'token' => env('EXCEPTION_NOTIFY_TELEGRAM_TOKEN'),
             ],
             'client' => [
-                'class' => Guanguans\Notify\Telegram\Client::class,
+                'class' => Notify\Telegram\Client::class,
             ],
             'message' => [
-                'class' => Guanguans\Notify\Telegram\Messages\TextMessage::class,
+                'class' => Notify\Telegram\Messages\TextMessage::class,
                 'options' => [
                     'chat_id' => env('EXCEPTION_NOTIFY_TELEGRAM_CHAT_ID'),
                     'text' => Template::CONTENT,
                 ],
             ],
             'pipes' => [
-                LimitLengthPipe::with(4096),
+                Pipes\LimitLengthPipe::with(4096),
             ],
         ],
 
         'weWork' => [
             'driver' => 'notify',
             'authenticator' => [
-                'class' => Guanguans\Notify\WeWork\Authenticator::class,
+                'class' => Notify\WeWork\Authenticator::class,
                 'token' => env('EXCEPTION_NOTIFY_WEWORK_TOKEN'),
             ],
             'client' => [
-                'class' => Guanguans\Notify\WeWork\Client::class,
+                'class' => Notify\WeWork\Client::class,
             ],
             'message' => [
-                'class' => Guanguans\Notify\WeWork\Messages\MarkdownMessage::class,
+                'class' => Notify\WeWork\Messages\MarkdownMessage::class,
                 'options' => [
                     'content' => Template::CONTENT,
                 ],
             ],
             'pipes' => [
-                SprintfMarkdownPipe::class,
-                LimitLengthPipe::with(4096),
+                Pipes\SprintfMarkdownPipe::class,
+                Pipes\LimitLengthPipe::with(4096),
             ],
         ],
     ],
